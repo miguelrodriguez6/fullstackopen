4a9b258df15b16d2903c71ed7353b37a8875cb11
miguelrodriguez6 <miguel.rodriguez6@udc.es>
2023-09-14 23:44:59 +0200

    1.11 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,21 +9,32 @@ const Header = ({ phrase }) => {
 
 const StatisticsLine = ({ type, num }) => {
   return (
-    <p>{type} {num}</p>
+    <tr>
+      <td>
+        {type}
+      </td>
+      <td>
+        {num}
+      </td>
+    </tr>
   )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
   let total = good + neutral + bad
+  if (total == 0) return <p>No feedback given</p>
   return (
-    <>
-      <StatisticsLine type='good' num={good} />
-      <StatisticsLine type='neutral' num={neutral} />
-      <StatisticsLine type='bad' num={bad} />
-      <StatisticsLine type='all' num={total} />
-      <StatisticsLine type='average' num={(good - bad) / (total)} />
-      <StatisticsLine type='positive' num={good / total} />
-    </>
+
+    <table>
+      <tbody>
+        <StatisticsLine type='good' num={good} />
+        <StatisticsLine type='neutral' num={neutral} />
+        <StatisticsLine type='bad' num={bad} />
+        <StatisticsLine type='all' num={total} />
+        <StatisticsLine type='average' num={((good - bad) / (total)).toFixed(1)} />
+        <StatisticsLine type='positive' num={`${(good / total).toFixed(1)}%`} />
+      </tbody>
+    </table>
   )
 
 }
@@ -53,18 +64,6 @@ const App = () => {
     setBad(bad + 1)
   }
 
-  if (good + neutral + bad == 0) {
-    return (
-      <div>
-        <Header phrase='give feedback' />
-        <Button handleClick={handleClickGood} word='good' />
-        <Button handleClick={handleClickNeutral} word='neutral' />
-        <Button handleClick={handleClickBad} word='bad' />
-        <Header phrase='statistics' />
-        <p>No feedback given</p>
-      </div>
-    )
-  }
 
   return (
     <div>
@@ -79,6 +78,4 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
